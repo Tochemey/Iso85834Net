9f981ff5ce7a479fb1f7f17a141b110d9ee014a3
bigtoch <tochemey@gmail.com>
2017-06-26 17:03:19 +0000

    Added a Readme and generate nuget package.

--- ModIso8583/Properties/AssemblyInfo.cs
+++ ModIso8583/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModIso8583")]
+[assembly: AssemblyTitle("Iso85834Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModIso8583")]
+[assembly: AssemblyProduct("Iso85834Net")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
